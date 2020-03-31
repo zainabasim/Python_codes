0370edeb1f63d7875b8dfa67c669850d08fd71ed
Zainab <62897671+zainabasim@users.noreply.github.com>
2020-03-31 16:46:51 +0500

    adding operators

---
+++ operators2.py
@@ -0,0 +1,14 @@
+zainab_age =13
+erum_age=26
+equalto=zainab_age==erum_age
+not_equalto=zainab_age!=erum_age
+less_than=zainab_age<erum_age
+greater_than=zainab_age>erum_age
+lessthan_equalto=zainab_age<=erum_age
+greaterthan_equalto=zainab_age>=erum_age
+print(equalto)
+print(not_equalto)
+print(less_than)
+print(greater_than)
+print(lessthan_equalto)
+print(greaterthan_equalto)
