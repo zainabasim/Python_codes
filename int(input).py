bff3981263f443190e3abaad6b9a186d192305fc
Zainab <62897671+zainabasim@users.noreply.github.com>
2020-04-01 21:11:30 +0500

    arthimatic operations

---
+++ int(input).py
@@ -0,0 +1,17 @@
+z=int(input("write the first value...."))
+a =int(input("write the second value...."))
+sum =z+a
+minus =z-a
+multiply =z*a
+divide =z/a
+remainder=z%a
+print("addition",sum)
+print("subtraction",minus)
+print("multiplication",multiply)
+print("division",divide)
+print("remainder",remainder
+
+
+
+
+
