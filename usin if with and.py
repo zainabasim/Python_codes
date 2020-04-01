3a7e6dd241841af57528fc0da5b5815c95c00892
Zainab <62897671+zainabasim@users.noreply.github.com>
2020-04-01 21:08:50 +0500

    adding if

---
+++ usin if with and.py
@@ -0,0 +1,8 @@
+Username_set="zainab"
+password_set="2006"
+
+username=input("enter your username")
+password=input("enter your password")
+
+if("Username_set==username")and("password_set==password"):
+    print("you log in succesfully!")
