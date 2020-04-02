21730be47295746be2fdf955a789008e7d3991b2
Zainab <62897671+zainabasim@users.noreply.github.com>
2020-04-02 13:52:15 +0500

    using if

---
+++ if condition.py
@@ -0,0 +1,7 @@
+watch =input("watch is expensive than glasses?(y/n)")
+glasses =input("glasses are cheaper than watch?(y/n)")
+
+if ("watch == Y"):
+    print("i like watches!")
+if ("glasses == Y"):
+    print("i like glasses!")
