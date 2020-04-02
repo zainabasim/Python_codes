c84ab0fb68a88b1dc10e587105de0b392ba304f0
Zainab <62897671+zainabasim@users.noreply.github.com>
2020-04-02 13:55:33 +0500

    and ,or

---
+++ using if with and ,or.py
@@ -0,0 +1,9 @@
+username_cap="Khadija"
+username_small="khadija"
+password_set="2343"
+
+usernamr=input("enter your username:")
+password=input("enter your password:")
+
+if("username_cap==username")or("username_small==username")and("password_set==password"):
+    print("you have login in successfully")
